9bce69ed69c1dc431c34a679709d6221296ff288
jooyoung9939 <155051230+jooyoung9939@users.noreply.github.com>
2024-03-23 21:02:41 +0900

    make project

---
+++ app/src/main/java/com/example/deckor_teamc_front/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.deckor_teamc_front
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
