5309afc4ff8a7fcca1a38d9e693c842d72170278
818jsy <818jsy72@gmail.com>
2024-08-14 04:33:37 +0900

    Feat: 지도추가

--- app/src/main/java/com/example/deckor_teamc_front/SVGFontResolver.kt
+++ app/src/main/java/com/example/deckor_teamc_front/SVGFontResolver.kt
@@ -10,6 +10,7 @@ class SVGFontResolver(private val context: Context) : SVGExternalFileResolver() 
     override fun resolveFont(fontFamily: String?, fontWeight: Int, fontStyle: String?): Typeface? {
         return when (fontFamily) {
             "나눔스퀘어 네오" -> ResourcesCompat.getFont(context, R.font.nanum_square_neo)
+            "Pretendard" -> ResourcesCompat.getFont(context, R.font.pretendard_semibold)
             else -> super.resolveFont(fontFamily, fontWeight, fontStyle)
         }
     }
