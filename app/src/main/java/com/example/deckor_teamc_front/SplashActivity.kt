6c0312c1d87440826c8d19c45f63c31f6e07381d
jooyoung9939 <155051230+jooyoung9939@users.noreply.github.com>
2024-08-16 16:35:42 +0900

    Feat: 스플래시 화면 추가

---
+++ app/src/main/java/com/example/deckor_teamc_front/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.deckor_teamc_front
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed(Runnable {
+            val i = Intent(this@SplashActivity,MainActivity::class.java)
+            startActivity(i)
+
+            finish()
+        }, 2000)
+    }
+}
