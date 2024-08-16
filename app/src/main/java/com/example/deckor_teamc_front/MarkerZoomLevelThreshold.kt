f995465bddd5d7633919a123427e0ead87e5cd20
818jsy <818jsy72@gmail.com>
2024-08-17 05:34:21 +0900

    Fix: 지도 이동 리스너 최적화

--- app/src/main/java/com/example/deckor_teamc_front/MarkerZoomLevelThreshold.kt
+++ app/src/main/java/com/example/deckor_teamc_front/MarkerZoomLevelThreshold.kt
@@ -3,7 +3,7 @@ package com.example.deckor_teamc_front
 object MarkerZoomLevelThreshold {
     // 줌 레벨에 대응하는 ID 리스트를 다양하게 배치합니다.
     val zoomToIdsMap: Map<Double, List<Int>> = mapOf(
-        14.0 to listOf(1, 8, 15, 22, 29, 36, 43, 50, 32),  // 일부 ID만 포함
+        1.0 to listOf(1, 8, 15, 22, 29, 36, 43, 50, 32),  // 일부 ID만 포함
         15.0 to listOf(2, 9, 16, 23, 30, 37, 44, 51, 3, 10, 17, 24, 31, 38, 45, 52, 4, 11, 18, 25, 32, 39, 46, 53), // 더 많은 ID 포함
         16.0 to (1..61).toList() // 모든 ID를 포함
     )
