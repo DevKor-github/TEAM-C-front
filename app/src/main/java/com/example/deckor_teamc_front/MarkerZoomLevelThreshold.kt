15497ba2f785e6db403d0b7a05818c954fee2e49
818jsy <818jsy72@gmail.com>
2024-08-17 18:52:23 +0900

    Feat: 창의관 공학관 산학관 추가 + SVG 수정

--- app/src/main/java/com/example/deckor_teamc_front/MarkerZoomLevelThreshold.kt
+++ app/src/main/java/com/example/deckor_teamc_front/MarkerZoomLevelThreshold.kt
@@ -3,7 +3,7 @@ package com.example.deckor_teamc_front
 object MarkerZoomLevelThreshold {
     // 줌 레벨에 대응하는 ID 리스트를 다양하게 배치합니다.
     val zoomToIdsMap: Map<Double, List<Int>> = mapOf(
-        1.0 to listOf(1, 8, 15, 22, 29, 36, 43, 50, 32),  // 일부 ID만 포함
+        1.0 to listOf(1, 8, 12, 15, 18, 20, 22, 29, 36, 43, 50, 32),  // 일부 ID만 포함
         15.0 to listOf(2, 9, 16, 23, 30, 37, 44, 51, 3, 10, 17, 24, 31, 38, 45, 52, 4, 11, 18, 25, 32, 39, 46, 53), // 더 많은 ID 포함
         16.0 to (1..61).toList() // 모든 ID를 포함
     )
@@ -17,6 +17,6 @@ object MarkerZoomLevelThreshold {
     }
 
     val availableBuildingList: List<Int> = listOf(
-        1, 2, 3, 10, 13, 14, 15, 17, 21, 25, 26, 29, 32, 33, 39, 42, 44, 45, 46, 49, 50, 53, 55, 56
+        1, 2, 3, 10, 12, 13, 14, 15, 17, 18, 20, 21, 25, 26, 29, 32, 33, 39, 42, 44, 45, 46, 49, 50, 53, 55, 56
     )
 }
