446e4e6a09c914cf433f42b15c1eba0839c0eedf
818jsy <818jsy72@gmail.com>
2024-08-20 03:50:30 +0900

    Fix: 패키지 이름(플레이스토어 배포하려면 기본 패키지 이름 사용불가) + 프로젝트 명 변경

---
+++ app/src/main/java/com/devkor/kodaero/Constants.kt
@@ -0,0 +1,6 @@
+package com.devkor.kodaero
+
+object Constants {
+    const val KU_PREFIX = "고려대학교 서울캠퍼스"
+    const val TAG_SUFFIX = "내부검색"
+}
---
+++ app/src/main/java/com/devkor/kodaero/OpenLocationModal.kt
@@ -0,0 +1,61 @@
+package com.devkor.kodaero
+
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.lifecycleScope
+import com.naver.maps.geometry.LatLng
+import kotlinx.coroutines.launch
+
+fun openLocationModal(activity: FragmentActivity, place: BuildingSearchItem) {
+
+    try {
+        // FragmentManager를 통해 HomeFragment를 찾거나 새로 생성
+        val fragmentManager = activity.supportFragmentManager
+        val homeFragment = fragmentManager.findFragmentByTag("HomeFragment") as HomeFragment?
+            ?: HomeFragment()
+
+
+
+        // HomeFragment가 이미 존재하면 popBackStack을 통해 해당 프래그먼트로 이동
+        if (homeFragment.isAdded) {
+            Log.d("openLocationModal", "HomeFragment already exists")
+            fragmentManager.popBackStack("SearchFragment", FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        } else {
+            // 존재하지 않으면 HomeFragment를 추가
+            fragmentManager.beginTransaction()
+                .replace(R.id.main_container, homeFragment, HomeFragment::class.java.simpleName)
+                .addToBackStack("HomeFragment")
+                .commit()
+            fragmentManager.executePendingTransactions()
+        }
+
+        // 카메라 위치 이동 및 모달 열기 작업
+        homeFragment.viewLifecycleOwner.lifecycleScope.launch {
+            homeFragment.moveCameraToPosition(LatLng(place.latitude ?: 38.0, place.longitude ?: 127.0))
+            homeFragment.updateSelectedBuilding(place.id)
+            homeFragment.view?.post {
+                val includedLayout = homeFragment.view?.findViewById<View>(R.id.includedLayout)
+                if (includedLayout == null) {
+                    Log.e("openLocationModal", "Included layout not found in HomeFragment layout")
+                    return@post
+                }
+
+                val nameTextView = includedLayout.findViewById<TextView>(R.id.modal_sheet_building_name)
+                val addressTextView = includedLayout.findViewById<TextView>(R.id.modal_sheet_building_address)
+
+                if (nameTextView == null || addressTextView == null) {
+                    Log.e("openLocationModal", "One of the TextViews not found")
+                    return@post
+                }
+
+                nameTextView.text = place.name
+                addressTextView.text = place.address
+            }
+        }
+    } catch (e: Exception) {
+        Log.e("openLocationModal", "Error in openLocationModal", e)
+    }
+}
