f7b80c5dca8900801681cb8a03810d620e748228
818jsy <818jsy72@gmail.com>
2024-08-24 05:56:14 +0900

    Fix: 내위치 아이콘

--- app/src/main/java/com/devkor/kodaero/PinSearchFragment.kt
+++ app/src/main/java/com/devkor/kodaero/PinSearchFragment.kt
@@ -411,8 +411,6 @@ class PinSearchFragment : Fragment(), OnMapReadyCallback {
         naverMap.locationSource = locationSource
         naverMap.uiSettings.isLocationButtonEnabled = true
 
-        naverMap.uiSettings.isLocationButtonEnabled = false
-
         naverMap.locationTrackingMode = LocationTrackingMode.NoFollow
 
         val initPosition = arguments?.getParcelable<LatLng>(ARG_INIT_CAMERA_POSITION) ?: initCameraPosition
