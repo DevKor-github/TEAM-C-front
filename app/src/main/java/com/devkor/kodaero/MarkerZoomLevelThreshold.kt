68d74683530c61fdbb70606e3509fd14e8fe2076
818jsy <818jsy72@gmail.com>
2024-08-24 06:16:14 +0900

    Feat: 중도 추가

--- app/src/main/java/com/devkor/kodaero/MarkerZoomLevelThreshold.kt
+++ app/src/main/java/com/devkor/kodaero/MarkerZoomLevelThreshold.kt
@@ -3,7 +3,7 @@ package com.devkor.kodaero
 object MarkerZoomLevelThreshold {
     // 줌 레벨에 대응하는 ID 리스트를 다양하게 배치합니다.
     val zoomToIdsMap: Map<Double, List<Int>> = mapOf(
-        1.0 to listOf(40, 15, 2, 32, 21, 14, 13, 33, 44, 10, 24, 3, 18),  // 일부 ID만 포함
+        1.0 to listOf(48, 15, 2, 32, 21, 14, 13, 33, 44, 10, 24, 3, 18),  // 일부 ID만 포함
         15.0 to listOf(7, 12, 20, 1, 26, 35, 45, 46, 49, 50, 53, 55), // 더 많은 ID 포함
         16.0 to (1..61).toList() // 모든 ID를 포함
     )
@@ -17,6 +17,6 @@ object MarkerZoomLevelThreshold {
     }
 
     val availableBuildingList: List<Int> = listOf(
-        1, 2, 3, 10, 12, 13, 14, 15, 17, 18, 20, 21, 25, 26, 29, 32, 33, 39, 40, 42, 44, 45, 46, 49, 50, 53, 55, 56
+        1, 2, 3, 10, 12, 13, 14, 15, 17, 18, 20, 21, 25, 26, 29, 32, 33, 39, 48, 42, 44, 45, 46, 49, 50, 53, 55, 56
     )
 }
